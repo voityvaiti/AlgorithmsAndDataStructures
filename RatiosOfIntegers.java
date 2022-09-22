6e0d16f03b9fd6224d06d0109ce9807cdf5993ed
voityvaiti <106198502+voityvaiti@users.noreply.github.com>
2022-09-22 12:37:08 +0200

    Update RatiosOfIntegers.java

--- RatiosOfIntegers.java
+++ RatiosOfIntegers.java
@@ -1,7 +1,7 @@
 /*Given an array of integers, function is calculating the ratios 
 of its elements, that are positive, negative and zero */
 
-public static void plusMinus(List<Integer> arr) {
+public static void ratiosOfIntegers(List<Integer> arr) {
     int positive = 0;
     int negative = 0;
     int zero = 0;
