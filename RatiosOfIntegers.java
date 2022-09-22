8f4c36e0e31edc5fb2c85c8c704fcd429685e892
voityvaiti <106198502+voityvaiti@users.noreply.github.com>
2022-09-22 12:36:02 +0200

    Create RatiosOfIntegers.java

---
+++ RatiosOfIntegers.java
@@ -0,0 +1,23 @@
+/*Given an array of integers, function is calculating the ratios 
+of its elements, that are positive, negative and zero */
+
+public static void plusMinus(List<Integer> arr) {
+    int positive = 0;
+    int negative = 0;
+    int zero = 0;
+    for(int value: arr) {
+        if(value>0){
+            positive++;
+        }
+        else if(value<0){
+            negative++;
+        }
+        else{
+            zero++;
+        }
+        }
+    int size = arr.size();
+    System.out.println((float)positive/size);
+    System.out.println((float)negative/size);
+    System.out.println((float)zero/size);
+    }
