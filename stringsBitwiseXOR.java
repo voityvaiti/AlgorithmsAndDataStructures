4f839c34ba22b015f3813aecb1ce8bdd458d0dcc
voityvaiti <106198502+voityvaiti@users.noreply.github.com>
2022-09-22 17:04:11 +0200

    Create stringsBitwiseXOR.java

---
+++ stringsBitwiseXOR.java
@@ -0,0 +1,14 @@
+/* Function returns string obtained by 
+the bitwise XOR of the two input strings */ 
+
+public static String stringsXOR(String s, String t) {
+        String res = new String("");
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == t.charAt(i))
+                res += "0";
+            else
+                res += "1";
+        }
+
+        return res;
+    }
